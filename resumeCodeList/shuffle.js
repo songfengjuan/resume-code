eacaee861254cd3abaffc97687139e86cb0a3608
songsong <songfengjuan@huohua.cn>
2021-02-08 17:36:57 +0800

    添加洗牌算法

---
+++ resumeCodeList/shuffle.js
@@ -0,0 +1,12 @@
+// 洗牌算法
+function getMess(arr) {
+    var n = arr.length;
+    var newArr = [];
+    while(n) {
+        // 随机获取一个数组下标
+        var i = Math.floor(Math.random()*n--);
+        // 把该随机下标对应的值push到新数组里面，原数组删除该值
+        newArr.push(arr.splice(i, 1)[0]);
+    }
+    return newArr;
+ }
